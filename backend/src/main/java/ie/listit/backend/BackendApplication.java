9b9a89dfc32d316eba8c36319adf5058886e5b0f
sahleyis <dahiruhammajam@gmail.com>
2026-09-11 00:23:53 +0100

    render fix

--- backend/src/main/java/ie/listit/backend/BackendApplication.java
+++ backend/src/main/java/ie/listit/backend/BackendApplication.java
@@ -2,7 +2,10 @@ package ie.listit.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +13,11 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("homeListings", "publicListings", "singleListing");
 	}
 
 }
